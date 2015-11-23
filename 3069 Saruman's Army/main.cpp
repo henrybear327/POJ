eb663cd91974da3ca0e5e595920bb1cacdfd7c0f
Chun-Hung Tseng <henrybear327@gmail.com>
2015-11-23 23:26:30 +0800

    Add POJ 3069

---
+++ 3069 Saruman's Army/main.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int R, n;
+    while (scanf("%d %d", &R, &n) == 2 && !(R == -1 && n == -1)) {
+        vector<int> pos;
+        for (int i = 0; i < n; i++) {
+            int curr;
+            scanf("%d", &curr);
+
+            pos.push_back(curr);
+        }
+
+        sort(pos.begin(), pos.end());
+
+        int cnt = 0, i = 0;
+        while (i < n) {
+            cnt++;
+            // find right-most point from left
+            int j = i;
+            while (j < n && pos[j + 1] - pos[i] <= R)
+                j++;
+            i = j;
+	    // find right-most point from center
+            while (j < n && pos[j + 1] - pos[i] <= R)
+                j++;
+            i = j + 1;
+        }
+
+        printf("%d\n", cnt);
+    }
+
+    return 0;
+}
